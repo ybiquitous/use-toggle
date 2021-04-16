7da1459da1f9ef831c1bf0116be594bfe61c1b8c
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-04-17 00:28:50 +0900

    fix: drop `toggle(boolean)` support (#6)
    This change allows the following better code:
    
    ```diff
    -<input type="checkbox" checked={checked} onChange={() => toggle()} />
    +<input type="checkbox" checked={checked} onChange={toggle} />
    ```

--- index.ts
+++ index.ts
@@ -4,24 +4,14 @@ export type ToggleOn = () => void;
 
 export type ToggleOff = () => void;
 
-export type Toggle = (nextState?: boolean) => void;
+export type Toggle = () => void;
 
 export default function useToggle(initial = false): [boolean, ToggleOn, ToggleOff, Toggle] {
   const [state, setState] = useState(initial);
 
-  const toggle: Toggle = useCallback(
-    (nextState) => {
-      if (typeof nextState === "boolean") {
-        setState(nextState);
-      } else {
-        setState((s) => !s);
-      }
-    },
-    [setState]
-  );
-
-  const toggleOn = useCallback(() => toggle(true), [toggle]);
-  const toggleOff = useCallback(() => toggle(false), [toggle]);
+  const toggleOn = useCallback(() => setState(true), [setState]);
+  const toggleOff = useCallback(() => setState(false), [setState]);
+  const toggle = useCallback(() => setState((s) => !s), [setState]);
 
   return [state, toggleOn, toggleOff, toggle];
 }
