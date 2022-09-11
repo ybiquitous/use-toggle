e59755ddd6af66dd2dd99d4a214d2214c18789b1
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-11 19:25:27 +0900

    refactor: test code

--- index.test.tsx
+++ index.test.tsx
@@ -6,77 +6,77 @@ import useToggle from "./index";
 test("initial state", () => {
   const Foo = () => {
     const [checked] = useToggle();
-    return <button>{`checked: ${checked}`}</button>;
+    return <button>{checked ? "checked" : "unchecked"}</button>;
   };
   render(<Foo />);
 
-  expect(screen.getByRole("button")).toHaveTextContent("checked: false");
+  expect(screen.getByRole("button")).toHaveTextContent("checked");
 });
 
 test("initial state with `true`", () => {
   const Foo = () => {
     const [checked] = useToggle(true);
-    return <button>{`checked: ${checked}`}</button>;
+    return <button>{checked ? "checked" : "unchecked"}</button>;
   };
   render(<Foo />);
 
-  expect(screen.getByRole("button")).toHaveTextContent("checked: true");
+  expect(screen.getByRole("button")).toHaveTextContent("checked");
 });
 
 test("initial state with `false`", () => {
   const Foo = () => {
     const [checked] = useToggle(false);
-    return <button>{`checked: ${checked}`}</button>;
+    return <button>{checked ? "checked" : "unchecked"}</button>;
   };
   render(<Foo />);
 
-  expect(screen.getByRole("button")).toHaveTextContent("checked: false");
+  expect(screen.getByRole("button")).toHaveTextContent("unchecked");
 });
 
 test("toggle on", () => {
   const Foo = () => {
     const [checked, check] = useToggle(false);
-    return <button onClick={check}>{`checked: ${checked}`}</button>;
+    return <button onClick={check}>{checked ? "checked" : "unchecked"}</button>;
   };
   render(<Foo />);
 
   const button = screen.getByRole("button");
 
   fireEvent.click(button);
-  expect(button).toHaveTextContent("checked: true");
+  expect(button).toHaveTextContent("checked");
 
   fireEvent.click(button);
-  expect(button).toHaveTextContent("checked: true"); // no change
+  expect(button).toHaveTextContent("checked"); // no change
 });
 
 test("toggle off", () => {
   const Foo = () => {
     const [checked, , uncheck] = useToggle(true);
-    return <button onClick={uncheck}>{`checked: ${checked}`}</button>;
+    return <button onClick={uncheck}>{checked ? "checked" : "unchecked"}</button>;
   };
   render(<Foo />);
 
   const button = screen.getByRole("button");
 
   fireEvent.click(button);
-  expect(button).toHaveTextContent("checked: false");
+  expect(button).toHaveTextContent("unchecked");
 
   fireEvent.click(button);
-  expect(button).toHaveTextContent("checked: false"); // no change
+  expect(button).toHaveTextContent("unchecked"); // no change
 });
 
 test("toggle switch", () => {
   const Foo = () => {
     const [checked, , , toggle] = useToggle(false);
-    return <button onClick={toggle}>{`checked: ${checked}`}</button>;
+    return <button onClick={toggle}>{checked ? "checked" : "unchecked"}</button>;
   };
   render(<Foo />);
 
   const button = screen.getByRole("button");
 
   fireEvent.click(button);
-  expect(button).toHaveTextContent("checked: true");
+  expect(button).toHaveTextContent("checked");
 
   fireEvent.click(button);
-  expect(button).toHaveTextContent("checked: false");
+  expect(button).toHaveTextContent("unchecked");
 });
