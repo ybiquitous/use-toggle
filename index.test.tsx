a124db382b415c09ada82879b921f3a5a77ae5f5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-11 18:25:28 +0900

    chore: update deps and tests
    `@testing-library/react-hooks` has not supported React 18 yet.
    So, this change replaces it with `@testing-library/react`.

---
+++ index.test.tsx
@@ -0,0 +1,82 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import useToggle from "./index";
+
+test("initial state", () => {
+  const Foo = () => {
+    const [checked] = useToggle();
+    return <div>{`checked: ${checked}`}</div>;
+  };
+  render(<Foo />);
+
+  expect(screen.getByText("checked: false")).toBeInTheDocument();
+});
+
+test("initial state with `true`", () => {
+  const Foo = () => {
+    const [checked] = useToggle(true);
+    return <div>{`checked: ${checked}`}</div>;
+  };
+  render(<Foo />);
+
+  expect(screen.getByText("checked: true")).toBeInTheDocument();
+});
+
+test("initial state with `false`", () => {
+  const Foo = () => {
+    const [checked] = useToggle(false);
+    return <div>{`checked: ${checked}`}</div>;
+  };
+  render(<Foo />);
+
+  expect(screen.getByText("checked: false")).toBeInTheDocument();
+});
+
+test("toggle on", () => {
+  const Foo = () => {
+    const [checked, check] = useToggle(false);
+    return <button onClick={check}>{`checked: ${checked}`}</button>;
+  };
+  render(<Foo />);
+
+  const button = screen.getByRole("button");
+
+  fireEvent.click(button);
+  expect(button).toHaveTextContent("checked: true");
+
+  fireEvent.click(button);
+  expect(button).toHaveTextContent("checked: true"); // no change
+});
+
+test("toggle off", () => {
+  const Foo = () => {
+    const [checked, _, uncheck] = useToggle(true);
+    return <button onClick={uncheck}>{`checked: ${checked}`}</button>;
+  };
+  render(<Foo />);
+
+  const button = screen.getByRole("button");
+
+  fireEvent.click(button);
+  expect(button).toHaveTextContent("checked: false");
+
+  fireEvent.click(button);
+  expect(button).toHaveTextContent("checked: false"); // no change
+});
+
+test("toggle switch", () => {
+  const Foo = () => {
+    const [checked, _check, _uncheck, toggle] = useToggle(false);
+    return <button onClick={toggle}>{`checked: ${checked}`}</button>;
+  };
+  render(<Foo />);
+
+  const button = screen.getByRole("button");
+
+  fireEvent.click(button);
+  expect(button).toHaveTextContent("checked: true");
+
+  fireEvent.click(button);
+  expect(button).toHaveTextContent("checked: false");
+});
